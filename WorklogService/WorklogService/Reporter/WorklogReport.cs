354ea4de98f1468d5b40eab81bc28dea826ce037
andras.nagy <andraas.naagy@gmail.com>
2022-02-19 20:55:47 +0100

    Add Initial version of WorklogReport

---
+++ WorklogService/WorklogService/Reporter/WorklogReport.cs
@@ -0,0 +1,32 @@
+﻿using WorklogService.DataAccess;
+using WorklogService.Models.Handler.Reports;
+using WorklogService.Models.Reports;
+
+namespace WorklogService.Reporter
+{
+    public class WorklogReport
+    {
+        private readonly List<IReportHandler> _reportHandlers;
+        private readonly IWorklogData _worklogData;
+
+        public WorklogReport(IWorklogData worklogData, IList<IReportHandler> reportHandlers)
+        {
+            _worklogData = worklogData;
+            _reportHandlers = new List<IReportHandler>(reportHandlers);
+        }
+
+        public Report CreateReport()
+        {
+            Report report = new Report();
+            foreach(var worklog in _worklogData.GetWorklogs())
+            {
+                foreach (var reportHandler in _reportHandlers)
+                {
+                    reportHandler.Add(worklog);
+                }
+            }
+            _reportHandlers.ForEach(s => s.Report(report));
+            return report;
+        }
+    }
+}
